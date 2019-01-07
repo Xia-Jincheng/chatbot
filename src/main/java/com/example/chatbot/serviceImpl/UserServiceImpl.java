bab81c7f502b2df03f18f5a8483696300d465691
OriginPrince <1492854075@qq.com>
2019-01-07 11:41:28 +0800

    initial step finished

---
+++ src/main/java/com/example/chatbot/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.chatbot.serviceImpl;
+
+import com.example.chatbot.entity.User;
+import com.example.chatbot.repository.UserRepository;
+import com.example.chatbot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUser(Integer id){
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void deleteUser(Integer id){
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    @Modifying
+    public void modifyUser(User user){
+        userRepository.save(user);
+    }
+}
