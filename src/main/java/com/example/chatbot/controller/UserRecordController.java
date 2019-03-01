a5b261ea7a2f71cb1e37c11bebf59c3c5ec5f355
xia-jincheng <310558084@qq.com>
2019-03-01 14:09:58 +0800

    background-manager

---
+++ src/main/java/com/example/chatbot/controller/UserRecordController.java
@@ -0,0 +1,19 @@
+package com.example.chatbot.controller;
+
+import com.example.chatbot.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRecordController {
+    @Autowired
+    RecordService recordService;
+
+    @GetMapping("/record_delete1/{id}")
+    public String deleteRecord(@PathVariable("id") Integer id){
+        recordService.deleteRecord(id);
+        return "成功删除用户该条聊天记录！";
+    }
+}
